a8086664a4a43e9714bb679fa84818d0a30fc822
Roee Shenberg <shenberg@gmail.com>
2018-04-13 19:54:11 +0300

    scripts to mass encode-decode

---
+++ decode_val.py
@@ -0,0 +1,7 @@
+import os
+import pathlib
+from tqdm import tqdm
+
+files = pathlib.Path('/home/shenberg/Documents/neural_compressor/enc_tmp').glob('*.enc')
+for file in tqdm(files):
+    os.system('python3 encdec.py -x "{}" "{}"'.format(str(file), '/home/shenberg/Documents/neural_compressor/dec_tmp' + file.stem + '.png'))
---
+++ encode_val.py
@@ -0,0 +1,7 @@
+import os
+import pathlib
+from tqdm import tqdm
+
+files = pathlib.Path('/home/shenberg/Documents/compression/valid').glob('*.png')
+for file in tqdm(files):
+    os.system('python3 encdec.py -c "{}" "{}"'.format(str(file), '/home/shenberg/Documents/neural_compressor/enc_tmp/' + file.stem + '.enc'))
