262dec412948d880eff450129a4443cf3d4acd7d
Roee Shenberg <shenberg@gmail.com>
2018-05-23 20:20:29 +0300

    use cuda in encode/decode for more speed

--- encode_val.py
+++ encode_val.py
@@ -4,4 +4,4 @@ from tqdm import tqdm
 
 files = pathlib.Path('/home/shenberg/Documents/compression/valid').glob('*.png')
 for file in tqdm(files):
-    os.system('python3 encdec.py -c "{}" "{}"'.format(str(file), '/home/shenberg/Documents/neural_compressor/enc_tmp/' + file.stem + '.enc'))
+    os.system('python3 encdec.py -c --cuda "{}" "{}"'.format(str(file), '/home/shenberg/Documents/neural_compressor/enc_tmp/' + file.stem + '.enc'))
